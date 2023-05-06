b9dd6b12284996997e22f0ef9e6be64f2d354e8f
developwizard <churkin.it@gmail.com>
2023-05-06 20:28:31 +0300

    Implemented service methods: getAll, getById, update, delete

--- src/main/java/com/developwizard/springbootbackend/service/EmployeeService.java
+++ src/main/java/com/developwizard/springbootbackend/service/EmployeeService.java
@@ -2,6 +2,45 @@ package com.developwizard.springbootbackend.service;
 
 import com.developwizard.springbootbackend.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
+
+    /**
+     * Saves employee into db
+     *
+     * @param employee Entity to be stored in the database
+     */
     Employee saveEmployee(Employee employee);
+
+    /**
+     * Retrieves all employees from db
+     *
+     * @return Employees list
+     */
+    List<Employee> getAllEmployees();
+
+    /**
+     * Retrieves employee from db
+     *
+     * @param id Employee ID
+     * @return Eployee
+     */
+    Employee getEmployeeById(long id);
+
+    /**
+     * Update employee by id
+     *
+     * @param employee New employee data
+     * @param id       Employee ID to update
+     * @return Updated employee
+     */
+    Employee updateEmployee(Employee employee, long id);
+
+    /**
+     * Remove employee from DB
+     *
+     * @param id Employee ID
+     */
+    void deleteEmployee(long id);
 }
--- src/main/java/com/developwizard/springbootbackend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/developwizard/springbootbackend/service/impl/EmployeeServiceImpl.java
@@ -1,13 +1,19 @@
 package com.developwizard.springbootbackend.service.impl;
 
 import com.developwizard.springbootbackend.entity.Employee;
+import com.developwizard.springbootbackend.exception.ResourceNotFoundException;
 import com.developwizard.springbootbackend.repository.EmployeeRepository;
 import com.developwizard.springbootbackend.service.EmployeeService;
+import lombok.val;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
+    private static final String EMPLOYEE = "Employee";
+    private static final String ID = "ID";
 
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
@@ -16,5 +22,30 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        return employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(EMPLOYEE, ID, id));
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee, long id) {
+        val existingEmployee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(EMPLOYEE, ID, id));
+        existingEmployee.setFirstName(employee.getFirstName());
+        existingEmployee.setLastName(employee.getLastName());
+        existingEmployee.setEmail(employee.getEmail());
+        return employeeRepository.save(existingEmployee);
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+        employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(EMPLOYEE, ID, id));
+        employeeRepository.deleteById(id);
     }
 }
