7c3fc4c9e1140725f15c68da8873df9f269f23af
developwizard <churkin.it@gmail.com>
2023-05-06 20:28:43 +0300

    Implemented controller methods: getAll, getById, update, delete

--- src/main/java/com/developwizard/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/developwizard/springbootbackend/controller/EmployeeController.java
@@ -4,22 +4,72 @@ import com.developwizard.springbootbackend.entity.Employee;
 import com.developwizard.springbootbackend.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
-    @PostMapping()
+    /**
+     * Create new employee
+     *
+     * @param employee Employee to be stored in the database
+     * @return Employee
+     */
+    @PostMapping
     public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
-        return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+        return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    /**
+     * Get all employees
+     *
+     * @return Employees list
+     */
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
+
+    /**
+     * Get employee by id
+     *
+     * @param id Employee ID
+     * @return Employee
+     */
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable("id") long id) {
+        return new ResponseEntity<>(employeeService.getEmployeeById(id), HttpStatus.OK);
+    }
+
+    /**
+     * Update existing employee
+     *
+     * @param id       Employee ID
+     * @param employee Employee data that need to be updated
+     * @return Updated employee
+     */
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id,
+                                                   @RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+    }
+
+    /**
+     * Remove employee
+     *
+     * @param id Employee ID
+     */
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>("Employee deleted successfully", HttpStatus.OK);
     }
 }
